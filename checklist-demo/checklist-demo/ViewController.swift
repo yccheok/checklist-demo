0baa6f1c5420c61aee1fde1266810a53f8faaca4
Yan Cheng Cheok <yancheng.cheok@gmail.com>
2021-10-02 14:59:43 +0800

    Initial creation
    Initial creation

---
+++ checklist-demo/checklist-demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  checklist-demo
+//
+//  Created by Yan Cheng Cheok on 02/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
