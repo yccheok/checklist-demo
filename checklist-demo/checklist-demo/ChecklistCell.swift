dd6e96ada77178fa8bb443cf6b5f8349d2b86c27
Yan Cheng Cheok <yancheng.cheok@gmail.com>
2021-10-02 15:09:52 +0800

    Checklist cell creation.
    Checklist cell creation.

---
+++ checklist-demo/checklist-demo/ChecklistCell.swift
@@ -0,0 +1,17 @@
+//
+//  ChecklistCell.swift
+//  checklist-demo
+//
+//  Created by Yan Cheng Cheok on 02/10/2021.
+//
+
+import UIKit
+
+class ChecklistCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
